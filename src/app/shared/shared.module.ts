dcbb32988515ed6600090f1a3cbcc1893c8f1a86
AxemaFr <axema@inbox.ru>
2020-05-23 14:53:21 +0400

    MedsolutionsTest/3:
    +created new-item.
    +refactored html code a little bit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewsListComponent } from './components/news-list/news-list.component';
+import { NewsItemComponent } from './components/news-item/news-item.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NewsListComponent, NewsItemComponent],
+  exports: [NewsListComponent],
   imports: [
     CommonModule
   ]
