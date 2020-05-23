bf08c41625a4669b1680ddea3296cd4d82987ed9
AxemaFr <axema@inbox.ru>
2020-05-23 16:26:27 +0400

    MedsolutionsTest/4:
    +added page-navigation
    +started to make news-page

---
+++ src/app/shared/components/page-navigation/page-navigation.component.ts
@@ -0,0 +1,75 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-page-navigation',
+  templateUrl: './page-navigation.component.html',
+  styleUrls: ['./page-navigation.component.scss']
+})
+export class PageNavigationComponent implements OnInit {
+
+  @Input() pagesCount = 7;
+  @Output() selected = new EventEmitter();
+
+  currentPage = 1;
+  startPage = 1;
+  pagesInRow = 5;
+  pages = [];
+
+  constructor() {
+    this.buildPages(0);
+  }
+
+  ngOnInit() {
+
+  }
+
+  buildPages(direction: number) {
+    this.currentPage += direction;
+    this.checkStartPage(direction);
+    this.pages = new Array(this.pagesInRow > this.pagesCount ? this.pagesCount : this.pagesInRow)
+      .fill(0)
+      .map( (page, index) => {
+      return index + this.startPage;
+    });
+  }
+
+  desc() {
+    this.buildPages(-1);
+  }
+
+  inc() {
+    this.buildPages(1);
+  }
+
+  checkStartPage(direction: number) {
+    if (direction > 0) {
+      if ((this.pages[this.pages.length - 1] - this.currentPage === 1)
+        && (this.currentPage < (this.pagesCount - 2))) {
+        this.startPage++;
+      }
+    } else {
+      if ((this.currentPage - this.pages[0] === 1)
+        && (this.currentPage > 2)) {
+        this.startPage--;
+      }
+    }
+  }
+
+  isPageVisible(type: string) {
+    if (type === 'first') {
+      return (this.currentPage > 3)&&(this.pagesCount > this.pagesInRow + 1);
+    }
+    if (type === 'last') {
+      return (this.pagesCount > this.pagesInRow);
+    }
+  }
+
+  isSeparatorVisible(type: string) {
+    if (type === 'first') {
+      return (this.currentPage > 4)&&(this.pagesCount > this.pagesInRow + 1);
+    }
+    if (type === 'last') {
+      return this.currentPage < (this.pagesCount - 3) && (this.pagesCount > this.pagesInRow + 1);
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsListComponent } from './components/news-list/news-list.component';
 import { NewsItemComponent } from './components/news-item/news-item.component';
+import { PageNavigationComponent } from './components/page-navigation/page-navigation.component';
+import { NewsPageComponent } from './components/news-page/news-page.component';
 
 
 
 @NgModule({
-  declarations: [NewsListComponent, NewsItemComponent],
+  declarations: [NewsListComponent, NewsItemComponent, PageNavigationComponent, NewsPageComponent],
   exports: [NewsListComponent],
   imports: [
     CommonModule
