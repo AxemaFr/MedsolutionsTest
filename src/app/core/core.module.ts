a6ca7e48de9a2f8d812dbda02c35a6514e75100c
AxemaFr <axema@inbox.ru>
2020-05-23 13:10:46 +0400

    MedsolutionsTest/2:
    +added sidebar
    +added sidebar-item

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, SidebarComponent],
+  exports: [HeaderComponent, SidebarComponent],
   imports: [
     CommonModule
   ]
